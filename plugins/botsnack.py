3cb1c8fff609d8328fb93e94b560565c0fb4808a
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2017-06-20 23:34:56 -0400

    create botsnack.py
    reward the bot with a snack

---
+++ plugins/botsnack.py
@@ -0,0 +1,21 @@
+from cloudbot import hook
+import random
+
+snacks = ['OMG THANKS!', 
+          'moar nummies!', 
+		  'OMNOMNOMNOM', 
+		  'much appreciated', 
+		  'AWESOMESAUCE', 
+		  'AWW YEE ( \u0361o \u035C\u0296 \u0361o)', 
+		  'THANKS!, check out these boobs \( o Y o )\ ', 
+		  'SNACK HARDER!', 
+		  'OMFG A FUCKING BOTSNACK!!1', 
+		  'I CAN HAZ SNACKS?', 
+		  'all your botsnacks are belong to me.', 
+		  'yummy thanks \:)'
+	]
+
+@hook.command("botsnack", "nummies", "moar nummies", "skittles", "jellybeans", "smores", "bubblegum", autohelp=False)
+def botsnack(message, conn):
+    """Reward the bot with a snack, snacks include: botsnack, nummies, moar nummies, skittles, jellybeans, smores, bubblegum."""
+    message(random.choice(snacks))
