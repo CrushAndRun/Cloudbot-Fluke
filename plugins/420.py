fb29d86c02b28f3d0d13ac4e406f7d7e74612fb0
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2017-06-21 00:05:21 -0400

    create 420.py
    get the bot high as a reward

---
+++ plugins/420.py
@@ -0,0 +1,21 @@
+from cloudbot import hook
+import random
+
+fourtwentys = ['OMG THANKS!', 
+               'grips it and rips it, THANKS!', 
+		       'puff puff pass!', 
+		       'much appreciated', 
+		       'AWESOMESAUCE', 
+		       'AWW YEE ( \u0361o \u035C\u0296 \u0361o)', 
+		       'THANKS!', 
+		       '420 HARDER!', 
+		       'OMFG ITS FUCKING 420!!1', 
+		       'I CAN HAZ WEED?', 
+		       'all your weeds are belong to me.', 
+		       'everybody must get stoned, sing it if you know the words! \:)'
+	]
+
+@hook.command("botsnack", "420", "bubbler", "bong", "MJ", "bonghit", "hash", autohelp=False)
+def 420(message, conn):
+    """Get the bot high with your choice: 420, bubbler, bong, MJ, bonghit, hash."""
+    message(random.choice(fourtwentys)
