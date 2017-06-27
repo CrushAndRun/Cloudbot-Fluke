c6b4aef7de6eb8524edb68f418d925855c8e44fc
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2017-06-27 15:19:21 -0400

    update to remove blank lines [grim]

--- plugins/print_url.py
+++ plugins/print_url.py
@@ -5,6 +5,10 @@ from cloudbot import hook
 
 # This will match ANY we url including youtube, reddit, twitch, etc... Some additional work needs to go into
 # not sending the web request etc if the match also matches an existing web regex.
+
+# 2017-06-26 Modified to exclude YouTube and twitter urls as they are handled by other modules. Also eliminated at link.
+# 2017-06-27 added code to remove blank lines from title if found.
+
 url_re = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
 
@@ -14,5 +18,9 @@ def print_url_title(match):
     r = requests.get(match.group())
     html = BeautifulSoup(r.text)
     title = html.title.text
-    out = "Title: {} at: {}".format(title, r.url)
-return out
+    # out = "Title: {} at: {}".format(title, r.url)
+    filtered = re.sub(u'(?imu)^\s*\n', u'', title)
+    if 'youtu' not in r.url and 'twitter' not in r.url:  #exclude YouTube URL's
+        out = "Title: {}".format(filtered)
+        return out
+    
