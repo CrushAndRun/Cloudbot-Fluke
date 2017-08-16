6228849e2038b40d63f692cbe677c77b95422e0d
deathspawn <deathspawn989@gmail.com>
2017-08-15 23:58:18 -0400

    Added headers & changed parsing

--- plugins/bellyup4blues.py
+++ plugins/bellyup4blues.py
@@ -8,15 +8,16 @@ import urllib.request
 
 url = "http://72.13.82.82:5100/7.html"
 
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)',
+        'Referer': 'http://www.thereeftank.com/forums/'}
 
 @hook.command("bu4b", autohelp=False)
 def bellyup4blues(text):
 
-    url = "http://72.13.82.82:5100/7.html"
-    html = urllib.request.urlopen(url).read()
-    htmlout = html[28:-15]
-    pw_bytes = htmlout.decode("utf-8")
-    filtered = pw_bytes.replace("&apos;", "'")
-    filtered = "Now Playing at http://bellyup4blues.com/: " + filtered
+    req = urllib.request.Request(url, data=None, headers=headers)
+    html = urllib.request.urlopen(req).read()
+    song = str(html).split("<body>")[1].split("</body>")[0].split(",")[6]
+    filtered = "Now Playing at http://bellyup4blues.com/: " + song
     out = filtered
     return out
