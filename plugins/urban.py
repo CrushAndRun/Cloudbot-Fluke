4459ab6af6282a390b1a6330e5c65d6e60395dca
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2018-07-20 17:08:05 -0400

    fix for UD fail per DS

--- plugins/urban.py
+++ plugins/urban.py
@@ -43,11 +43,11 @@ def urban(text):
 
         page = request.json()
 
-        try:
-            if page['result_type'] == 'no_results':
-                return 'Not found.'
-        except:
-            pass
++        try:
++            if page['result_type'] == 'no_results':
++                return 'Not found.'
++        except:
++            pass
     else:
         # get a random definition!
         try:
