b90e1ce09358d0cff0c9f893c372037e7a2f08aa
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2019-12-21 20:18:23 -0500

    create reallibertymedia.py

---
+++ plugins/reallibertymedia.py
@@ -0,0 +1,126 @@
+#reallibertymedia.py script
+from cloudbot import hook
+
+@hook.command("rlm_home", autohelp=False)
+def rlm_home():
+    """link to RLM home page [shortened]"""
+    return "RLM home page -> https://bit.ly/2SgM2wk"
+	
+@hook.command("rlm_donate", autohelp=False)
+def rlm_donate():
+    """link to RLM donate page [shortened]"""
+    return "RLM donate page -> https://bit.ly/36ZWHzW"
+	
+@hook.command("grim_xyz", autohelp=False)
+def grim_xyz():
+    """link to Grimnir's Demension home page"""
+    return "Grimnir's Demension -> https://grimnir.xyz/"
+	
+@hook.command("rlm_channels", autohelp=False)
+def rlm_channels():
+    """link to RLM channels page [shortened]"""
+    return "RLM channels page -> https://bit.ly/35NR9rQ"
+	
+@hook.command("rlm_twitter", autohelp=False)
+def rlm_twitter():
+    """link to RLM Twitter page [shortened]"""
+    return "RLM Twitter page -> https://bit.ly/391lrtl"
+	
+@hook.command("rlm_tweets", autohelp=False)
+def rlm_tweets():
+    """link to RLM Tweeted Times page [shortened]"""
+    return "RLM Tweeted Times page -> https://bit.ly/2Qdj4uI"
+	
+@hook.command("rlm_facebook", autohelp=False)
+def rlm_facebook():
+    """link to RLM FaceBook page [shortened]"""
+    return "RLM facebook page -> https://bit.ly/2sWll5t"
+	
+@hook.command("rlm_about", autohelp=False)
+def rlm_about():
+    """link to RLM about page [shortened]"""
+    return "RLM about page -> https://bit.ly/2Mk6okB"
+	
+@hook.command("rlm_tumblr", autohelp=False)
+def rlm_tumblr():
+    """link to RLM Tumblr page [shortened]"""
+    return "RLM Tumblr page -> https://bit.ly/2s8EAc0"
+	
+@hook.command("rlm_blog", autohelp=False)
+def rlm_blog():
+    """link to RLM Blog page[all] [shortened]"""
+    return "RLM Blog *all* ->https://bit.ly/2QdXhmC"
+	
+@hook.command("rlm_shows", autohelp=False)
+def rlm_shows():
+    """link to RLM shows page [shortened]"""
+    return "RLM show pages -> https://bit.ly/2EElRb8"
+	
+@hook.command("rlm_podcast", autohelp=False)
+def rlm_podcast():
+    """link to RLM Podcast page [shortened]"""
+    return "RLM Podcasts -> https://bit.ly/393YUMk"
+	
+@hook.command("rlm_shop", autohelp=False)
+def rlm_shop():
+    """link to RLM shopping outlets amazon/cafepress pages [shortened]"""
+    return "amazon -> https://amzn.to/2PMExMj cafepress -> https://bit.ly/2EEr7eT"
+	
+@hook.command("rlm_support", autohelp=False)
+def rlm_support():
+    """link to RLM support [crypto] page [shortened]"""
+    return "RLM support via Crypto -> https://bit.ly/36ZrQTX"
+	
+@hook.command("rlm_wire", autohelp=False)
+def rlm_wire():
+    """link to Wire download/support pages [shortened]"""
+    return "Wire download -> https://bit.ly/2tC04hS Wire support -> https://bit.ly/2PM6UKo"
+	
+@hook.command("rlm_kiwi", autohelp=False)
+def rlm_kiwi():
+    """link to RLM Kiwi login chat page [shortened]"""
+    return "RLM Kiwi Pop-Up -> https://bit.ly/2sTq0Fi"
+	
+@hook.command("rlm_radio", autohelp=False)
+def rlm_radio():
+    """link to RLM Radio access pages [shortened]"""
+    return "RLM-PopUp -> https://bit.ly/2Mm8qkq Tunein -> https://bit.ly/2EIBZbA Internet Radio -> https://bit.ly/2s0Yfed"
+	
+@hook.command("rlm_quotes", autohelp=False)
+def rlm_quotes():
+    """link to RLM quotes database page [shortened]"""
+    return "RLM Quote Database -> https://bit.ly/2Zf3Olp"
+	
+@hook.command("rlm_youtube", autohelp=False)
+def rlm_youtube():
+    """link to RLM YouTube page [shortened]"""
+    return "RLM YouTube page -> https://bit.ly/2rYCpYJ"
+	
+@hook.command("rlm_bitchute", autohelp=False)
+def rlm_bitchute():
+    """link to RLM Bitchute page [shortened]"""
+    return "RLM Bitchute page -> https://bit.ly/2sQckLu"
+	
+@hook.command("rlm_minds", autohelp=False)
+def rlm_minds():
+    """link to RLM Minds page [shortened]"""
+    return "RLM Minds page -> https://bit.ly/35It3yK"
+	
+@hook.command("rlm_org", autohelp=False)
+def rlm_org():
+    """link to RLM.org page [shortened]"""
+    return "link to reallibertymedia.org -> https://bit.ly/2PLc35l"
+	
+@hook.command("script_list", autohelp=False)
+def script_list():
+    """list of all the commands in the reallibertymedia.py script enter .script_list"""
+    return "rlm_+ org|minds|bitchute|youtube|quotes|radio|kiwi|wire|support|shop|podcast|shows|blog|tumblr|about|facebook|tweets|twitter|channels|donate|home. enter .grim_xyz as a separate cmd."
+**********************************************************************************	
+
+#Example script below:
+
+#@hook.command("cmd_here", autohelp=False)
+#def cmd here():
+#    """help info here """
+#    return "output here"
+	
